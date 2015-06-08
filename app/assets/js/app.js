7690ea7ea7f286f9cce90369d009326ed482ab00
Andrew Savelev <savelevcorr@gmail.com>
2015-06-08 15:16:00 +1000

    Remove requirejs config

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,25 +1,7 @@
-require.conf({
+require.config({
 	baseUrl: "src/bower/",
 
 	paths: {
 		"jquery" : "jquery/jquery"
 	}
 });
-
-define(["jquery"], function ($) {
-	if($)
-	console.log($);
-});
-/*
-echo "# Gulping" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin git@github.com:savelevcorr/Gulping.git
-git push -u origin master
-…or push an existing repository from the command line
-
-
-git remote add origin git@github.com:savelevcorr/Gulping.git
-git push -u origin master
-*/
